87e6ddb01e9654b719f834a6b25b97df7cb72101
Yiyang Wang <wang.yiyang7@northeastern.edu>
2025-01-13 14:21:09 -0800

    modified:   App.tsx 	new file:   components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+interface HeaderProps {
+  name: string;
+}
+
+//const Header = (props: { name: string }) => {
+const Header = (props: HeaderProps) => {
+  console.log(props);
+  return (
+    <View>
+      <Text>Welcome to {props.name}</Text>
+    </View>
+  );
+};
+
+export default Header;
