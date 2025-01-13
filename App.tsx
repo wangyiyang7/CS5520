59047410ea7b91b38ec8beedf9d1705ec404b06f
Yiyang Wang <wang.yiyang7@northeastern.edu>
2025-01-13 14:49:01 -0800

    modified:   App.tsx 	new file:   components/Input.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import Input from "./components/Input";
 import { useState } from "react";
 
 export default function App() {
-  const [txt, setTxt] = useState("");
-
   const appName = "OnePlus APP";
   return (
     <View style={styles.container}>
       <Header name={appName} />
       <StatusBar style="auto" />
-      <TextInput
-        placeholder="place holder"
-        onChangeText={(changedTxt: string) => setTxt(changedTxt)}
-        value={txt}
-      />
+      <Input />
     </View>
   );
 }
