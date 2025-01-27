99ee20e540ae51433e9277d3a885d11e3171de19
Yiyang Wang <wang.yiyang7@northeastern.edu>
2025-01-27 15:49:48 -0800

    modified:   App.tsx 	new file:   components/GoalItem.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import { useState } from "react";
+import GoalItem from "./components/GoalItem";
 
-interface Goal {
+export interface Goal {
   id: number;
   text: string;
 }
@@ -37,6 +46,10 @@ export default function App() {
     //console.log("!!!??" + modalVisible);
   }
 
+  function handleDelete(deleteNum: number) {
+    console.log(deleteNum);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
@@ -54,16 +67,31 @@ export default function App() {
       />
       <View style={styles.bottomContainer}>
         {inputText.length > 0 ? (
-          <View>
+          <FlatList
+            contentContainerStyle={{ alignItems: "center" }}
+            data={goalList}
+            renderItem={({ item }) => {
+              return (
+                /*
+                <View style={styles.userTyped}>
+                  <Text style={{ fontSize: 80 }}>{item.text}</Text>
+                </View>*/
+                <GoalItem item={item} delete={() => handleDelete(item.id)} />
+              );
+            }}
+          />
+        ) : /*
+          <ScrollView contentContainerStyle={{ alignItems: "center" }}>
             {goalList.map((x) => {
               return (
                 <View key={x.id} style={styles.userTyped}>
-                  <Text>{x.text}</Text>
+                  <Text style={{ fontSize: 80 }}>{x.text}</Text>
                 </View>
               );
             })}
-          </View>
-        ) : null}
+          </ScrollView>
+          */
+        null}
       </View>
     </SafeAreaView>
   );
@@ -84,14 +112,14 @@ const styles = StyleSheet.create({
   bottomContainer: {
     flex: 4,
     backgroundColor: "#dcd",
-    alignItems: "center",
+    //alignItems: "center",
   },
   userTyped: {
-    marginTop: 10,
+    marginTop: 30,
     backgroundColor: "#fff8dc",
     borderRadius: 5,
     padding: 10,
     fontSize: 20,
-    borderWidth: 1,
+    //borderWidth: 1,
   },
 });
