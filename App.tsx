1bcc9c99f9369acac61bf9c0f57736b6082a9e75
Yiyang Wang <wang.yiyang7@northeastern.edu>
2025-01-27 14:06:03 -0800

    List

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import { useState } from "react";
+
+interface Goal {
+  id: number;
+  text: string;
+}
 
 export default function App() {
   const appName = "Balding APP";
   let autofocus: boolean = true;
   const [inputText, setInputText] = useState("");
   const [modalVisible, setVisible] = useState(false);
+  const [goalList, setGoalList] = useState<Goal[]>([]);
 
+  //function handleInputData(inputText: string) {
+  //setInputText(inputText);
   function handleInputData(inputText: string) {
     setInputText(inputText);
+    let newGoal: Goal = { text: inputText, id: Math.random() };
+    //setGoalList([...goalList, newGoal]);
     setVisible(false);
+    setGoalList((prev) => {
+      return [...prev, newGoal];
+    });
   }
 
   function handleVisibleTrue() {
@@ -48,8 +54,14 @@ export default function App() {
       />
       <View style={styles.bottomContainer}>
         {inputText.length > 0 ? (
-          <View style={styles.userTyped}>
-            <Text>{inputText}</Text>
+          <View>
+            {goalList.map((x) => {
+              return (
+                <View key={x.id} style={styles.userTyped}>
+                  <Text>{x.text}</Text>
+                </View>
+              );
+            })}
           </View>
         ) : null}
       </View>
@@ -75,9 +87,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   userTyped: {
-    margin: 10,
+    marginTop: 10,
     backgroundColor: "#fff8dc",
     borderRadius: 5,
     padding: 10,
+    fontSize: 20,
+    borderWidth: 1,
   },
 });
